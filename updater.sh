89e9c5f4f1a659d9296d1c9731d48368996e6c7a
SkylerHope <elpidakouro13@gmail.com>
2026-02-27 23:59:25 +0200

    changes

--- updater.sh
+++ updater.sh
@@ -1,1 +1,5 @@
 #!/bin/bash
+
+# Compare current version with latest
+current_version="https://github.com/stoatchat/for-desktop/releases/download/v1.3.0/Stoat-linux-x64-1.3.0.zip"
+new_version=$(curl -sI https://github.com/stoatchat/for-desktop/releases/download//Stoat-linux-x64-.zip | grep -oP 'download/v//\K[0-9]+\.[0-9]+\.[0-9]+' | grep -oP  'linux-x64-/\K[0-9]+\.[0-9]+\.[0-9]+')
